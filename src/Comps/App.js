ff47eefa26e70160661579b02d3350fe06483793
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2021-05-12 16:45:07 +0530

    animation

--- src/Comps/App.js
+++ src/Comps/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import NoteApp from './NoteComps/1_NoteApp';
+import React, { lazy, Suspense } from 'react';
+// import NoteApp from './NoteComps/1_NoteApp';
+const NoteApp = lazy(() => import('./NoteComps/1_NoteApp'));
 
 const App = () => {
-    return <NoteApp />;
+    return (
+        <Suspense
+            fallback={
+                <h1
+                    style={{
+                        fontSize: '4rem',
+                        textAlign: 'center',
+                        height: '100vh',
+                        width: '100vw',
+                        color: 'black',
+                    }}
+                >
+                    Loading...
+                </h1>
+            }
+        >
+            <NoteApp />
+        </Suspense>
+    );
 };
 
 export default App;
